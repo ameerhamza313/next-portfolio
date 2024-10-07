65ac1d7bc5bf054129f31a66e8cfc69a3b8fe49e
ameerhamza313 <ameerhamzasanadi@gmail.com>
2024-10-07 11:12:02 +0530

    Update next config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withBundleAnalyzer({
-  basePath: process.env.BASE_PATH || "/next-portfolio",
-  output: "export",
-  // output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+  basePath: "/next-portfolio",
+  // output: "export",
+  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
   reactStrictMode: true,
   pageExtensions: ["ts", "tsx", "js"],
   eslint: {
